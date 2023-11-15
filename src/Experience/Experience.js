252b82fc3620d7f2894baff1770fa145bf438151
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-11-15 20:41:58 +0200

    created the scene

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,3 +1,4 @@
+import * as THREE from "three";
 import Sizes from "./Utils/Sizes";
 import Time from "./Utils/Time.js";
 
@@ -12,11 +13,16 @@ export default class Experience {
     //Setup
     this.sizes = new Sizes();
     this.time = new Time();
-
+    this.scene = new THREE.Scene();
     // Resize event
     this.sizes.on("resize", () => {
       this.resize();
     });
+    // Time tick event
+    this.time.on("tick", () => {
+      this.update();
+    });
   }
   resize() {}
+  update() {}
 }
