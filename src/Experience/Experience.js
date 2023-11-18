686576461730d4012adee3aa7e599a5ccdb573f0
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-11-18 22:11:52 +0200

    Debug

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,11 +1,12 @@
 import * as THREE from "three";
-import Sizes from "./Utils/Sizes";
+import Sizes from "./Utils/Sizes.js";
 import Time from "./Utils/Time.js";
 import Camera from "./Camera.js";
 import Renderer from "./Renderer.js";
-import World from "./World/World";
-import Resources from "./Utils/Resources";
-import sources from "./sources";
+import World from "./World/World.js";
+import Resources from "./Utils/Resources.js";
+import sources from "./sources.js";
+import Debug from "./Utils/Debug.js";
 
 let instance = null;
 
@@ -23,6 +24,7 @@ export default class Experience {
     this.canvas = canvas;
 
     //Setup
+    this.debug = new Debug();
     this.sizes = new Sizes();
     this.time = new Time();
     this.scene = new THREE.Scene();
