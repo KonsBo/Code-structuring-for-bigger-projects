3fe1858ff732241d0dbd13163c28898eb5f165eb
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-11-18 20:02:59 +0200

    Added floor

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,12 +1,15 @@
 import * as THREE from "three";
+import { PositionalAudio } from "three";
 import Experience from "../Experience.js";
 import Environment from "./Environment.js";
+import Floor from "./Floor.js";
 
 export default class World {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
+
     //Test mesh
     const testMesh = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
@@ -17,6 +20,7 @@ export default class World {
     //Wait for resources
     this.resources.on("ready", () => {
       //Setup
+      this.floor = new Floor();
       this.environment = new Environment();
     });
   }
