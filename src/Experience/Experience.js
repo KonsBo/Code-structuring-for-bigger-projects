1f1e13ddebdc8fe57e08d498c9e8a6c076b7680b
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-11-18 13:50:19 +0200

    added resources class/ instantiate loaders

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -4,6 +4,8 @@ import Time from "./Utils/Time.js";
 import Camera from "./Camera.js";
 import Renderer from "./Renderer.js";
 import World from "./World/World";
+import Resources from "./Utils/Resources";
+import sources from "./sources";
 
 let instance = null;
 
@@ -24,6 +26,7 @@ export default class Experience {
     this.sizes = new Sizes();
     this.time = new Time();
     this.scene = new THREE.Scene();
+    this.resources = new Resources(sources);
     this.camera = new Camera();
     this.renderer = new Renderer();
     this.world = new World();
